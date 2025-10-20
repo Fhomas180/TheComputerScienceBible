2880989f2d68d8a73bd7317f7df82aed39bde434
Thomas Flavin <160640300+Fhomas180@users.noreply.github.com>
2025-10-20 16:27:54 -0400

    Add files via upload

---
+++ TheComputerScienceBible/TheBibleProgrammingFromCollege/TheBibleProgrammingFromCollege/AdvancedProgramming.cpp
@@ -0,0 +1,11 @@
+#include "AdvancedProgramming.h"
+
+Advanced::Advanced()
+{
+	cout << "Welcome To Advanced Programming" << endl;
+
+}
+
+Advanced::~Advanced()
+{
+}
---
+++ TheComputerScienceBible/TheBibleProgrammingFromCollege/TheBibleProgrammingFromCollege/IntroToProgramming.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Beginning {
+public:
+	Beginning();
+	~Beginning();
+	void Variables();
+	void Expressions();
+	void Syntax();
+	void Decisions();
+	void Loops();
+	void FileIO();
+	void Functions();
+	void Arrays();
+private:
+	int select = 0;
+
+
+
+};
