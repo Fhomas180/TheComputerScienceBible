0b931ab957023feced31a71bb257bad1f81da9c1
Thomas Flavin <160640300+Fhomas180@users.noreply.github.com>
2025-10-23 13:54:29 -0400

    Add files via upload

---
+++ ProgrammingCollegeBible/ProgrammingCollegeBible/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "IntroToProgramming.h"
+#include "AdvancedProgramming.h"
+using namespace std;
+
+
+int main() {
+	int pick = 0;
+	cout << "Welcome To Computer Science Notes\n" << endl;
+	cout << "Objective: To Note Anything From Freshmen Year To Senior Year\n" << endl;
+	do {
+		cout << "Choose One By Putting In A Number" << endl;
+		cout << "1. Intro To Programming" << endl;
+		cout << "2. Advanced Programming" << endl;
+		cout << "3. Data Structures & Algorithms" << endl;
+		cout << "4. Database Mangement Systems" << endl;
+		cout << "5. Intro To Modern Graphics" << endl;
+		cout << "6. Game Architecture" << endl;
+		cout << "7. Computer Architecture" << endl;
+		cout << "8. Artifical Intelligence For Games" << endl;
+		cout << "9. Quit" << endl;
+		cin >> pick;
+	} while (pick < 1 || pick > 9);
+	switch (pick) {
+	case 1:
+		Beginning::Beginning();
+		break;
+	case 2:
+		Advanced::Advanced();
+		break;
+
+	}
+
+
+
+
+
+}
